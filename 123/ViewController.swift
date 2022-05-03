00782962d237aa7a18b1b5983f31eb9359ea8963
Гость <Guest@iMac-Mac005-4.local>
2022-05-03 16:55:03 +0900

    first commit

--- 123/ViewController.swift
+++ 123/ViewController.swift
@@ -1,22 +1,22 @@
-//
-//  ViewController.swift
-//  123
-//
-//  Created by Гость on 28.04.2022.
-//
-
 import UIKit
-import  CoreLocation
+import CoreLocation
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    
     let locationManager = CLLocationManager()
+    var weatherData = WeatherData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         startLocationManager()
     }
-
+    
     func startLocationManager() {
         locationManager.requestWhenInUseAuthorization()
         
@@ -25,27 +25,43 @@ class ViewController: UIViewController {
             locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
             locationManager.pausesLocationUpdatesAutomatically = false
             locationManager.startUpdatingLocation()
+            }
+        
+        }
+    
+        func updateView() {
+            cityName.text = weatherData.name
+            weatherDescriptionLabel.text = DataSource.weatherIDs[weatherData.weather[0].id]
+            temperatureLabel.text = weatherData.main.temp.description + "°"
+            weatherIconImageView.image = UIImage(named: weatherData.weather[0].icon)
     }
-}
+    
     func updateWeatherInfo(latitude: Double, longtitude: Double) {
         let session = URLSession.shared
-        let url = URL(string: "975041f1055df194576152c76120121b")!
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude.description)&lon=\(longtitude.description)&units=metric&lang=ru&appid=4ea971b2229bae81f7487b817bb857db")!
         let task = session.dataTask(with: url) { (data, response, error) in
             guard error == nil else {
                 print("DataTask error: \(error!.localizedDescription)")
                 return
             }
+            
             do {
+                self.weatherData = try JSONDecoder().decode(WeatherData.self, from: data!)
+                DispatchQueue.main.async {
+                    self.updateView()
+                }
             } catch {
                 print(error.localizedDescription)
             }
         }
+        task.resume()
+    }
 }
-}
+
 extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let lastLocation = locations.last{
-            print(lastLocation.coordinate.latitude, lastLocation.coordinate.longitude)
+        if let lastLocation = locations.last {
+            updateWeatherInfo(latitude: lastLocation.coordinate.latitude, longtitude: lastLocation.coordinate.longitude)
         }
     }
 }
